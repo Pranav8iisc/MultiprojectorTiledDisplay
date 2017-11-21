5d58d79046210c4c6c10c3f06a6821b5e55b7a26
pranavkantgaur <pranav8iisc@gmail.com>
2017-11-21 16:29:16 +0530

    WIP: code cleanup

--- bin/Debug/Hosts.py
+++
@@ -1,3 +0,0 @@
-MOTHERSHIP_HOST = 'gscore4.compunet.barc.in'
-HOSTS = ['gscore1.compunet.barc.in','gscore1.compunet.barc.in','gscore1.compunet.barc.in','gscore2.compunet.barc.in','gscore2.compunet.barc.in','gscore2.compunet.barc.in','gscore3.compunet.barc.in','gscore3.compunet.barc.in','gscore3.compunet.barc.in']
-DISPLAY_PORT = ['0.0','0.1','0.2','0.0','0.1','0.2','0.0','0.1','0.2']
